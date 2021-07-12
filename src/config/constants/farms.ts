c2d6b7e506fa4c562f78aae78d86f6491d7495f5
BscClassic <ah0229247@gmail.com>
2021-07-12 22:43:54 +0700

    first file

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -26,10 +26,10 @@ const farms: FarmConfig[] = [
       97: '',
       56: '0xd1b59d11316e87c3a0a069e80f590ba35cd8d8d3',
     },
-    tokenSymbol: 'egg',
+    tokenSymbol: 'BSX',
     tokenAddresses: {
       97: '',
-      56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+      56: '0x31834433C5B5D679Bf9041F3D4e57Bb1ead15532',
     },
     quoteTokenSymbol: QuoteToken.BNB,
     quoteTokenAdresses: contracts.wbnb,
