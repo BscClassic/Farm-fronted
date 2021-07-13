44bcff52d9df605d6a35732ac71c935b8240cee6
BscClassic <85980463+BscClassic@users.noreply.github.com>
2021-07-13 19:46:18 +0700

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -12,11 +12,11 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Exchange',
-        href: 'https://exchange.goosedefi.com/',
+        href: 'https://bscclassic.org/#/swap',
       },
       {
         label: 'Liquidity',
-        href: 'https://exchange.goosedefi.com/#/pool',
+        href: 'https://bscclassic.org/#/pool',
       },
     ],
   },
@@ -51,20 +51,20 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+        href: 'https://pancakeswap.info/',
       },
       {
         label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/goose-finance',
+        href: 'https://www.coingecko.com/en/coins/',
       },
       {
         label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/goose-finance/',
+        href: 'https://coinmarketcap.com/currencies/',
       },
-      {
-        label: 'AstroTools',
-        href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
-      },
+      // {
+      //   label: 'AstroTools',
+      //   href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
+      // },
     ],
   },
   {
@@ -73,12 +73,12 @@ const config: MenuEntry[] = [
     items: [
       {
         label: 'Github',
-        href: 'https://github.com/goosedefi/',
+        href: 'https://github.com/BscClassic',
       },
-      {
-        label: 'Docs',
-        href: 'https://goosedefi.gitbook.io/goose-finance/',
-      },
+      // {
+      //   label: 'Docs',
+      //   href: 'https://goosedefi.gitbook.io/goose-finance/',
+      // },
       {
         label: 'Blog',
         href: 'https://goosefinance.medium.com/',
@@ -93,12 +93,16 @@ const config: MenuEntry[] = [
   {
     label: 'Audit by Hacken',
     icon: 'AuditIcon',
-    href: 'https://www.goosedefi.com/files/hackenAudit.pdf',
+    href: '/Audit',
   },
   {
     label: 'Audit by CertiK',
     icon: 'AuditIcon',
-    href: 'https://certik.org/projects/goose-finance',
+    href: 'https://certik.org/',
+    status: {
+      text: t('Soon').toLocaleUpperCase(),
+      color: 'success',
+    },
   },
 ]
 
