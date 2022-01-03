521b4d1208a9f411f32f9a1ea69bea3cbb935cf1
Your Name <you@example.com>
2022-01-03 17:00:38 +0700

    file

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -45,76 +45,76 @@ const config: MenuEntry[] = [
     icon: 'PoolIcon',
     href: '/nests',
   },
-  {
-  label: 'IFO',
-  icon: 'IfoIcon',
-  href: '/ifo',
-},
-  {
-   label: 'Lottery',
-   icon: 'TicketIcon',
-   href: '/lottery',
-  },
+  // {
+  // label: 'IFO',
+  // icon: 'IfoIcon',
+  // href: '/ifo',
+  // },
+  // {
+  // label: 'Lottery',
+  //  icon: 'TicketIcon',
+  //  href: '/lottery',
+  // },
   // {
   //   label: 'NFT',
   //   icon: 'NftIcon',
   //   href: '/nft',
   // },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/',
-      },
-      {
-        label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/',
-      },
-      {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/',
-      },
+  // {
+  //   label: 'Info',
+  //   icon: 'InfoIcon',
+  //   items: [
+  //     {
+  //       label: 'PancakeSwap',
+  //       href: 'https://pancakeswap.info/',
+  //     },
+  //     {
+  //       label: 'CoinGecko',
+  //       href: 'https://www.coingecko.com/en/coins/',
+  //     },
+  //     {
+  //       label: 'CoinMarketCap',
+  //       href: 'https://coinmarketcap.com/currencies/',
+  //     },
       // {
       //   label: 'AstroTools',
       //   href: 'https://app.astrotools.io/pancake-pair-explorer/0x19e7cbecdd23a16dfa5573df54d98f7caae03019',
       // },
-    ],
-  },
-  {
-    label: 'More',
-    icon: 'MoreIcon',
-    items: [
-      {
-        label: 'Github',
-        href: 'https://github.com/BscClassic',
-      },
+  //   ],
+  // },
+  // {
+  //   label: 'More',
+  //   icon: 'MoreIcon',
+  //   items: [
+  //     {
+  //       label: 'Github',
+  //       href: 'https://github.com/BscClassic',
+  //     },
       // {
       //   label: 'Docs',
       //   href: 'https://goosedefi.gitbook.io/goose-finance/',
       // },
-      {
-        label: 'Blog',
-        href: 'https://goosefinance.medium.com/',
-      },
-    ],
-  },
-  {
-    label: 'Partnerships/IFO',
-    icon: 'GooseIcon',
-    href: 'https://farm.bscclassic.org',
-  },
-  {
-    label: 'Audit by Hacken',
-    icon: 'AuditIcon',
-    href: '.',
-  },
-  {
-    label: 'Audit by CertiK',
-    icon: 'AuditIcon',
-    href: 'https://certik.org/',
-  },
+  //    {
+  //      label: 'Blog',
+  //      href: 'https://goosefinance.medium.com/',
+  //    },
+  //  ],
+  // },
+  // {
+  //   label: 'Partnerships/IFO',
+  //   icon: 'GooseIcon',
+  //   href: 'https://farm.bscclassic.org',
+  // },
+  // {
+  //   label: 'Audit by Hacken',
+  //   icon: 'AuditIcon',
+  //   href: '.',
+  // },
+  // {
+  //   label: 'Audit by CertiK',
+  //   icon: 'AuditIcon',
+  //   href: 'https://certik.org/',
+  // },
 ]
 
 export default config
